f5c570e6494a21f3d400573647a6484ab66aed20
Erofey Bashunov <cannor147@gmail.com>
2020-09-30 05:35:36 +0300

    fix(hw03): remove duplicated code of database connection
    (cherry picked from commit 9b7fada3c18ec2da11a33508a0dc703cebb4838a)

--- hw03-refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/ProductDao.java
+++ hw03-refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/ProductDao.java
@@ -9,101 +9,78 @@ import java.util.Optional;
 
 public class ProductDao {
     public void createTable() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        executeUpdate("CREATE TABLE IF NOT EXISTS PRODUCT" +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                " NAME           TEXT    NOT NULL, " +
+                " PRICE          INT     NOT NULL)");
     }
 
     public List<Product> findAll() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
-            rs.close();
-            stmt.close();
-
-            List<Product> products = new ArrayList<>();
-            while (rs.next()) {
-                Product product = new Product();
-                product.setName(rs.getString("name"));
-                product.setPrice(rs.getInt("price"));
-                products.add(product);
-            }
-            return products;
+        ResultSet rs = executeQuery("SELECT * FROM PRODUCT");
+        List<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getInt("price"));
+            products.add(product);
         }
+        return products;
     }
 
     public Optional<Product> findMax() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-            rs.close();
-            stmt.close();
-
-            if (rs.next()) {
-                Product product = new Product();
-                product.setName(rs.getString("name"));
-                product.setPrice(rs.getInt("price"));
-                return Optional.of(product);
-            } else {
-                return Optional.empty();
-            }
+        ResultSet rs = executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
+        if (rs.next()) {
+            Product product = new Product();
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getInt("price"));
+            return Optional.of(product);
+        } else {
+            return Optional.empty();
         }
     }
 
     public Optional<Product> findMin() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-            rs.close();
-            stmt.close();
-
-            if (rs.next()) {
-                Product product = new Product();
-                product.setName(rs.getString("name"));
-                product.setPrice(rs.getInt("price"));
-                return Optional.of(product);
-            } else {
-                return Optional.empty();
-            }
+        ResultSet rs = executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
+        if (rs.next()) {
+            Product product = new Product();
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getInt("price"));
+            return Optional.of(product);
+        } else {
+            return Optional.empty();
         }
     }
 
     public int sumPrices() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-            rs.close();
-            stmt.close();
-
-            return rs.next() ? 0 : rs.getInt(1);
-        }
+        ResultSet rs = executeQuery("SELECT SUM(price) FROM PRODUCT");
+        return rs.next() ? 0 : rs.getInt(1);
     }
 
     public int count() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-            rs.close();
-            stmt.close();
-
-            return rs.next() ? 0 : rs.getInt(1);
-        }
+        ResultSet rs = executeQuery("SELECT COUNT(*) FROM PRODUCT");
+        return rs.next() ? 0 : rs.getInt(1);
     }
 
     public void insert(Product product) throws SQLException {
+        executeUpdate("INSERT INTO PRODUCT" +
+                "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")");
+    }
+
+    private ResultSet executeQuery(String query) throws SQLException {
+        ResultSet resultSet;
         try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "INSERT INTO PRODUCT " +
-                    "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")";
-            Statement stmt = c.createStatement();
-            stmt.executeUpdate(sql);
-            stmt.close();
+            Statement statement = c.createStatement();
+            resultSet = statement.executeQuery(query);
+            statement.close();
+        }
+        return resultSet;
+    }
+
+    private void executeUpdate(String query) throws SQLException {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement statement = c.createStatement();
+            statement.executeQuery(query);
+            statement.close();
         }
     }
 }
