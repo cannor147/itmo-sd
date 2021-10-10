c194154107fa8c0aabe33110c94e29e56f9c75e6
Erofey Bashunov <cannor147@gmail.com>
2021-10-11 01:24:40 +0300

    feat(lab04): initial commit

---
+++ lab04-mvc/src/main/java/com/github/cannor147/itmo/software/lab04/TaskMvcApplication.java
@@ -0,0 +1,11 @@
+package com.github.cannor147.itmo.software.lab04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMvcApplication.class, args);
+    }
+}
