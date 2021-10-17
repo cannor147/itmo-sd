cb126edac782590b5f2e89414994c33abafc7839
Erofey Bashunov <cannor147@gmail.com>
2021-10-17 06:33:23 +0300

    feat(lab05): implemented drawing api with awt and javafx

--- lab05-bridge/src/main/java/com/github/cannor147/itmo/software/lab05/Application.java
+++ lab05-bridge/src/main/java/com/github/cannor147/itmo/software/lab05/Application.java
@@ -1,7 +1,15 @@
 package com.github.cannor147.itmo.software.lab05;
 
+import com.github.cannor147.itmo.software.lab05.api.AwtDrawingApi;
+import com.github.cannor147.itmo.software.lab05.api.DrawingApi;
+import com.github.cannor147.itmo.software.lab05.api.FxDrawingApi;
+
 public class Application {
     public static void main(String[] args) {
-        // No operations.
+//        final DrawingApi drawingApi = new FxDrawingApi(600, 600);
+        final DrawingApi drawingApi = new AwtDrawingApi(600, 600);
+        drawingApi.drawCircle(0, 0, 100);
+        drawingApi.drawCircle(200, 200, 100);
+        drawingApi.show();
     }
 }
--- lab05-bridge/src/main/java/com/github/cannor147/itmo/software/lab05/api/DrawingApi.java
+++ lab05-bridge/src/main/java/com/github/cannor147/itmo/software/lab05/api/DrawingApi.java
@@ -5,4 +5,5 @@ public interface DrawingApi {
     long getDrawingAreaHeight();
     void drawCircle(double x, double y, double radius);
     void drawLine(double x1, double y1, double x2, double y2);
+    void show();
 }
