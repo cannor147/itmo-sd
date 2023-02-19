a8d2040144d0ac9fda2ac7d3e89ba963bfadd35e
Erofey Bashunov <cannor147@gmail.com>
2023-02-19 18:18:21 +0400

    feat(lab10): added readme and created spring project

---
+++ lab10-events/src/main/kotlin/com/github/cannor147/itmo/sd/lab10/FitnessApplication.kt
@@ -0,0 +1,11 @@
+package com.github.cannor147.itmo.sd.lab10
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab10EventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab10EventsApplication>(*args)
+}
