44fe7d5f10c5fe25cb59ff73186dba55c2d8d677
Erofey Bashunov <cannor147@gmail.com>
2023-03-27 05:52:54 +0300

    feat(lab11): created exchange project

---
+++ lab11-integration/exchange/src/main/kotlin/com/github/cannor147/itmo/sd/lab11/ExchangeApplication.kt
@@ -0,0 +1,11 @@
+package com.github.cannor147.itmo.sd.lab11
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExchangeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExchangeApplication>(*args)
+}
